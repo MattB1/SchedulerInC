d5f73106534f2ba4c5bd30ca82534e4f6156c4a7
Matthew Bubb <mbubb2@myune.edu.au>
2018-09-12 15:43:25 +1000

    round robin done

--- main.c
+++ main.c
@@ -19,7 +19,11 @@ struct record {
     long processing;
     long timestarted;
     long timefinished;
+    long remaining;
 };
+
+
+
 
 void checkNumberofProcesses(int numProcess){
     if (numProcess != 0 && numProcess != 1 && numProcess != 2 && numProcess != 3 && numProcess != 4
@@ -39,8 +43,7 @@ void checkProcessingTime(long processTime){
     }
 }
 
-void FCFS(int numProcess);
-void SRT(int numProcess);
+
 
 
 void checkDup(long size, long pid[] ){
@@ -71,6 +74,10 @@ int cmp(const void *a,const void *b)
     else if((*a1).value>(*a2).value)return 1;
     else return 0;
 }
+
+void FCFS(int numProcess);
+void SRT(int numProcess);
+void RR(int numProcess, int quantum);
 
 
 
@@ -118,7 +125,6 @@ int main(int argc, char *argv[]) {
     //they previously compiled)
     checkFileArg(argc);
 
-
     FILE *fp;
     char numOfProcess;
 
@@ -147,13 +153,13 @@ int main(int argc, char *argv[]) {
             processing[i] = ProcessingTime;
             // end getting items.
 
-            checkProcessingTime(processing[i]);
-            checkDup(sizeof(pid[i]), pid);
+            checkProcessingTime(processing[i]);//checks the processing time is greater than 0
+            checkDup(sizeof(pid[i]), pid); //checks for any duplicate processing id's
 
             //get total processing time;
             totalprotime += ProcessingTime;
 
-            //check which arrived first, WORKING!//the running list is important for the quicksort
+            //check which arrived first//the running list is important for the quicksort
             if(arrival[i] < arrival[i-1] || arrival[i] == 0 ) {
                 runningFirst = pid[i];
             }
@@ -177,6 +183,7 @@ int main(int argc, char *argv[]) {
             array[i].pid = pid[i];
             array[i].arrival = arrival[i];
             array[i].processing = processing[i];
+            array[i].remaining = processing[i];
         }
 
         // SHORTEST RESPONSE TIME NEXT
@@ -190,6 +197,8 @@ int main(int argc, char *argv[]) {
         // remember_processtime[] = array[].processing;
         // wt[] = 0;
         // timer = 0;
+        RR(numProcess, 2);
+        RR(numProcess, 4);
 
 
         fclose(fp);
@@ -198,12 +207,14 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+
 void FCFS(int numProcess){
     struct record array[numProcess];
     for(int i=0;i<numProcess;i++) {
         array[i].pid = pid[i];
         array[i].arrival = arrival[i];
         array[i].processing = processing[i];
+
     }
 
     struct str objects[numProcess];
@@ -245,8 +256,7 @@ void FCFS(int numProcess){
     }
     printf("Average waiting time: %.2f\n", waitingTime);
 
-
-    //calculate turnaround time (finish time - arrival time)...total time from arrival until completition)
+    //calculate turnaround time (finish time - arrival time)...total time from arrival until completion)
     for (int i = 0; i < numProcess;i++){
         turnaroundTime[i] = array[i].timefinished - array[i].arrival;
         //printf("turnaround time for process %d: %.2f\n", i, turnaroundTime[i]);
@@ -272,17 +282,13 @@ void SRT(int numProcess) {
         objects2[i].value = array[i].processing;
         objects2[i].index=i;
     }
-    //sort objects array according to value maybe using qsort
+    //sort objects array according to value using qsort
     qsort(objects2,numProcess,sizeof(objects2[0]),cmp);
-
 
     printf("\nSRT\nTime\tPID\n");
 
-
-
     int a[10],b[10],x[10],i,smallest,count=0,times,n;
     double avg=0,tt=0,end;
-
     n = numProcess;
 
     for(i=0;i<n;i++)
@@ -316,4 +322,148 @@ void SRT(int numProcess) {
     printf("Average Turnaround time = %.2lf\n",tt/n);
 
 }
-//This is a comment to check my git commits
+
+void RR(int numProcess, int quantum){
+
+
+    struct record array[numProcess];
+    for(int i=0;i<numProcess;i++) {
+        array[i].pid = pid[i];
+        array[i].arrival = arrival[i];
+        array[i].processing = processing[i];
+        array[i].remaining = processing[i];
+    }
+
+
+
+    struct str objects[numProcess];
+    for(int i=0;i<numProcess;i++)
+    {
+        objects[i].value=runningList[i];
+        objects[i].index=i;
+    }
+    //sort objects array according to value maybe using qsort
+    qsort(objects,numProcess,sizeof(objects[0]),cmp);
+
+    /*for (int i = 0; i < numProcess; i++) {
+        int correct = objects[i].index;
+        for (int j = 0; j < array[correct].processing; j++) {
+            printf("%d\t%li\n", time, array[correct].pid);
+            time++;
+            array[correct].timestarted = time  - j;
+            array[correct].timefinished = time;
+
+
+        }
+    }*/
+
+    //have a priority list. lowest 'array[i].pid' to highest
+    //have a queue [PID 3: 1, PID 1: 2, PID 2: 3]
+    //print first in queue for qantum
+    //when qantum is over check if another pid has arrived
+        //if one has check whether it's pid is lower or high than current
+        //if one hasn't then print the current pid for it's remaining processing time.
+    /*int remaining;
+    int current;
+    int time = 0;
+    quantum = 2;
+    int remainingTime[100];
+
+
+        for (int i = 0; i < numProcess; i++) {
+            array[i].remaining = array[i].processing;
+            //printf("remaining[%d] = %d\n", i, array[i].remaining);
+        }
+        for (int i = 0; i < numProcess; i++) {
+            remaining = array[i].remaining;
+            current = array[i].pid;
+            //printf("array[%d].remaining = %d\n", i, remaining);
+            for (int k = remaining; k > 0; k--) {
+                remaining = remaining-1;
+                for (int p = 0; p<2; p++){
+                    printf("current: %d\n", current);
+
+
+                    }
+                    if(remaining>0){
+                        current = array[i+1].pid;
+                    }else if(remaining==0){
+
+                    }
+
+                }
+                printf("printing remaining of pid[%d]: %d\n", current, remaining);
+                time++;
+            }*/
+
+
+
+
+
+
+
+
+
+
+        int priority[100];
+
+    /*or(int i = 0; i<numProcess; i++){
+        priority[i] = array[i].pid;
+        printf("priority[%d] = %d\n", i, priority[i]);
+        printf("%d\t%li\n", i, array[objects[i].index].pid);
+        //if(priority[i]
+    }*/
+    //for(int i = 0; i< remaining[i]; i++) {
+
+
+
+
+    int i,j,n,time,remain,flag=0,ts;
+    ts = quantum;
+    int sum_wait=0,sum_turnaround=0,at[10],bt[10],rt[10],pn[10];
+    printf("Enter no of Processes : ");
+    scanf("%d",&n);
+    remain=n;
+    for(i=0;i<n;i++) {
+        printf("Enter  arrival time and burst time for Process P%d :",i+1);
+        pn[i] = i;
+        scanf("%d",&at[i]);
+        scanf("%d",&bt[i]);
+        rt[i]=bt[i];
+    }
+    for(i = 0;i<n;i++){
+        printf("%d\n", pn[i]);
+    }
+    //printf("Enter time slice");
+    //scanf("%d",&ts);
+    printf("\n\nProcess\t|Turnaround time|waiting time\n\n");
+    for(time=0,i=0;remain!=0;){
+        if(rt[i]<=ts && rt[i]>0){
+            time+=rt[i];
+            rt[i]=0;
+            flag=1;
+        } else if(rt[i]>0){
+            rt[i]-=ts;
+            time+=ts;
+        }
+
+        if(rt[i]==0 && flag==1){
+            remain--;
+            printf("P[%d]\t|\t%d\t|\t%d\n",i+1,time-at[i],time-at[i]-bt[i]);
+            sum_wait+=time-at[i]-bt[i]-1;
+            sum_turnaround+=time-at[i];
+            flag=0;
+        }if(i==n-1){
+            i=0;
+        }else if(at[i+1]<time){
+            i++;
+        }else{
+            i=0;
+        }
+    }
+    printf("\nAvg sum_wait = %f\n",sum_wait*1.0/n);
+    printf("Avg sum_turnaround = %f",sum_turnaround*1.0/n);
+
+
+
+}
